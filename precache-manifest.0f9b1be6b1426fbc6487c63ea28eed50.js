329f2da6854cae2b289ff7e8a8a9fc2fb5d34737
Harsh Chandra <harshchandrahc5@gmail.com>
2020-10-19 23:52:37 +0100

    Updates

---
+++ precache-manifest.0f9b1be6b1426fbc6487c63ea28eed50.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "85c6a2d706ee8d5dc10c3335bdccbad1",
+    "url": "/react-todo/index.html"
+  },
+  {
+    "revision": "68ff10225487281fd282",
+    "url": "/react-todo/static/css/main.056d7525.chunk.css"
+  },
+  {
+    "revision": "72f59a7a602bf9aa9151",
+    "url": "/react-todo/static/js/2.a4548b3b.chunk.js"
+  },
+  {
+    "revision": "b13e737c23346402089bf79c557ecf58",
+    "url": "/react-todo/static/js/2.a4548b3b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "68ff10225487281fd282",
+    "url": "/react-todo/static/js/main.fa51f970.chunk.js"
+  },
+  {
+    "revision": "b81e150b308975bc2389",
+    "url": "/react-todo/static/js/runtime-main.89ae8f88.js"
+  }
+]);
